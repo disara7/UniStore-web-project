d8453bcf711132610a980a9fcca137815d833256
disara7 <129250994+disara7@users.noreply.github.com>
2024-01-18 00:44:13 +0530

    added home screen images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Nav from './Components/Nav/nav';
+import Home from './Components/home/home';
 
 function App() {
   return (
     <div>
       <Nav/>
+      <Home />
       
     </div>
   );
---
+++ src/Components/home/home.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './home.css'
+
+const Home = () => {
+  return (
+    <section>
+        <div class="banner">
+            <div class="image-container">
+                <div class="image" id="preloved">
+                    
+                </div>
+                
+            </div>
+            {/* <div className="heading1">
+                    <h1>PRE-LOVED</h1>
+                </div> */}
+            <div class="image-container">
+                <div class="image" id="craftsworld">
+                
+                </div>
+                
+            </div>
+            {/* <div className="heading2">
+                    <h1>CRAFTS-WORLD</h1>
+                </div> */}
+        </div>
+    </section>
+  )
+}
+
+export default Home
