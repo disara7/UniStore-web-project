6b68bed4561de0ad0dc501bb1fdbf3adff38525f
NavinduD <r.l.t.n.rathnayaka@gmail.com>
2024-01-25 21:19:52 +0530

    Home responsiveness is completed. auto slider couldn't finish.(for <768px)

--- src/Components/popular/popular.jsx
+++ src/Components/popular/popular.jsx
@@ -1,21 +1,52 @@
-import React from 'react'
-import './popular.css'
-import data_product from '../Assets/data'
-import Item from '../items/item'
+import React, { useEffect, useState } from 'react';
+import './popular.css';
+import data_product from '../Assets/data';
+import Item from '../items/item';
 
 const Popular = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prevSlide) => (prevSlide + 1) % data_product.length);
+    }, 3000); // Change slide every 3 seconds
+  
+    return () => clearInterval(interval);
+  }, [data_product.length]);
+
   return (
     <div className='popular'>
-        <h1>Top Picks for You</h1>
-        <hr />
+      <h1>Top Picks for You</h1>
+      <hr />
+      <div className='slideshow'>
         <div className="popular-item">
-            {data_product.map((item, i)=>{
-                return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
-            })}
+          {data_product.map((item, i) => (
+            <Item
+              className={`slide ${currentSlide === i ? 'active' : ''}`}
+              key={i}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              new_price={item.new_price}
+              old_price={item.old_price}
+            />
+          ))}
         </div>
-
+        {window.innerWidth < 768 && (
+        <div className="dots">
+          {data_product.map((_, i) => (
+            <button
+              key={i}
+              className={`dot ${currentSlide === i ? 'active' : ''}`}
+              onClick={() => setCurrentSlide(i)}
+            >
+              {'•'}
+            </button>
+          ))}
+        </div>)}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Popular
+export default Popular;
