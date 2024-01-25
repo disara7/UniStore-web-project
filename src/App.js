97c74eb17fe494cc7aadcd441b9b6f77c675c361
disara7 <129250994+disara7@users.noreply.github.com>
2024-01-26 00:02:31 +0530

    changed files

--- src/App.js
+++ src/App.js
@@ -10,11 +10,7 @@ import Cart from './Pages/home/cart';
 import Login from './Pages/home/login';
 import CraftsWorld from './Pages/home/CraftsWorld/craftsworld';
 import Footer from './Components/footer/Footer';
-<<<<<<< Updated upstream
 import CreateAccount from './Pages/home/createaccount';
-=======
-import CreateAccount from './Pages/home/createacc';
->>>>>>> Stashed changes
 
 
 function App() {
@@ -32,11 +28,7 @@ function App() {
             <Route path=':productId' element={<Product/>} />
           </Route>
           <Route path='/cart' element={<Cart/>}/> 
-<<<<<<< Updated upstream
-          <Route path='/login' element={<Login/>}/>
-=======
           <Route path='/login' element={<Login/>}/> 
->>>>>>> Stashed changes
           <Route path='/createaccount' element={<CreateAccount/>}/>         
         </Routes>
         <Footer/>
