6b27f33726b00caea58e9d6bbb30861df0a88350
disara7 <129250994+disara7@users.noreply.github.com>
2024-02-19 12:22:26 +0530

    added more images and adjusted breadcrumbs

--- src/Components/Assets/all_items.js
+++ src/Components/Assets/all_items.js
@@ -1,6 +1,7 @@
 import cw1 from '../Assets/images/cw1.png'
 import pl1 from '../Assets/images/pl1.png'
 import pl2 from '../Assets/images/pl2.png'
+import cw2 from '../Assets/images/cw2.png'
 
 let all_items = [
     {
@@ -30,34 +31,18 @@ let all_items = [
         old_price: "LKR 100.00",
     },
     {
-        id: 3,
-        name: "Physics 2nd Year PST | Hydro Dynamics Short Note",
-        category: "preloved",
-        image: pl2,
-        new_price: "LKR 75.00",
-        old_price: "LKR 100.00",
-    },
-    {
-        id: 3,
-        name: "Physics 2nd Year PST | Hydro Dynamics Short Note",
-        category: "preloved",
-        image: pl2,
-        new_price: "LKR 75.00",
-        old_price: "LKR 100.00",
-    },
-    {
         id: 4,
-        name: "Scrunchie Lab | Colorful Handmade Scrunchies",
-        category: "craftsworld",
-        image: cw1,
+        name: "Physics 2nd Year PST | Hydro Dynamics Short Note",
+        category: "preloved",
+        image: pl2,
         new_price: "LKR 75.00",
         old_price: "LKR 100.00",
     },
     {
         id: 5,
-        name: "Scrunchie Lab | Colorful Handmade Scrunchies",
-        category: "craftsworld",
-        image: cw1,
+        name: "Physics 2nd Year PST | Hydro Dynamics Short Note",
+        category: "preloved",
+        image: pl2,
         new_price: "LKR 75.00",
         old_price: "LKR 100.00",
     },
@@ -66,6 +51,22 @@ let all_items = [
         name: "Scrunchie Lab | Colorful Handmade Scrunchies",
         category: "craftsworld",
         image: cw1,
+        new_price: "LKR 75.00",
+        old_price: "LKR 100.00",
+    },
+    {
+        id: 7,
+        name: "Scrunchie Lab | Colorful Handmade Scrunchies",
+        category: "craftsworld",
+        image: cw1,
+        new_price: "LKR 75.00",
+        old_price: "LKR 100.00",
+    },
+    {
+        id: 8,
+        name: "Scrunchie Lab | Colorful Handmade Scrunchies",
+        category: "craftsworld",
+        image: cw2,
         new_price: "LKR 75.00",
         old_price: "LKR 100.00",
     },
