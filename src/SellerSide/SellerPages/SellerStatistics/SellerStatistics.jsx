c6de741cb6276513c51f42519fba745f0fd8040d
DushyanthaAT <dushyantha721@gmail.com>
2024-02-29 20:58:15 +0530

    Add Seller statistic page

--- src/SellerSide/SellerPages/SellerStatistics/SellerStatistics.jsx
+++ src/SellerSide/SellerPages/SellerStatistics/SellerStatistics.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import './SellerStatistics.css'
+import SellerNav from "../../SellerComponents/SellerNav/SellerNav";
+import SellerStatTable from "../../SellerComponents/statTable/sellerStatTable";
+
 const SellerStatistic = () => {
   return (
-    <div>
-      <h1>stat</h1>
+    <div className="background">
+      <SellerNav />
+      <div className="container">
+        <div className="leftbox">
+          <SellerStatTable />
+        </div>
+        <div className="rightbox">
+          <h4>Total Earnings</h4>
+          <hr />
+          <h2>LKR 4231.00</h2>
+          <div className="payoutBtn">
+            <button className="submitBtn" type="submit">
+              Payout
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default SellerStatistic
