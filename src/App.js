76c06ed7e76023f770a90b66921f99a36a66a83a
DushyanthaAT <dushyantha721@gmail.com>
2024-04-07 10:30:26 +0530

    as

--- src/App.js
+++ src/App.js
@@ -5,22 +5,23 @@ import Preloved from './Pages/home/Preloved/preloved';
 import About from './Pages/about';
 import Contact from './Pages/contact';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Product from './Pages/home/product';
-import Cart from './Pages/home/cart';
-import Login from './Pages/home/login';
-import CraftsWorld from './Pages/home/CraftsWorld/craftsworld';
-import Footer from './Components/footer/Footer';
-import CreateAccount from './Pages/home/createaccount';
-import Finish from './Pages/home/signupfinish';
-import Item from './Components/items/item';
-import ProductDisplay from './Components/ProductDisplay/product_display';
-import unistorecontextProvider from './Context/unistorecontext';
-import Payment from './Pages/Payment/payment';
-import SellerNav from './SellerSide/SellerComponents/SellerNav/SellerNav';
-import SellerDashboard from './SellerSide/SellerPages/SellerDashboard/SellerDashboard';
-import SellerStatistic from './SellerSide/SellerPages/SellerStatistics/SellerStatistics';
-import SellerUploadProduct from './SellerSide/SellerPages/SellerUploadProduct/SellerUploadProduct';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./Pages/home/product";
+import Cart from "./Pages/home/cart";
+import Login from "./Pages/home/login";
+import CraftsWorld from "./Pages/home/CraftsWorld/craftsworld";
+import Footer from "./Components/footer/Footer";
+import CreateAccount from "./Pages/home/createaccount";
+import Finish from "./Pages/home/signupfinish";
+import Item from "./Components/items/item";
+import ProductDisplay from "./Components/ProductDisplay/product_display";
+import unistorecontextProvider from "./Context/unistorecontext";
+import Payment from "./Pages/Payment/payment";
+import SellerNav from "./SellerSide/SellerComponents/SellerNav/SellerNav";
+import SellerDashboard from "./SellerSide/SellerPages/SellerDashboard/SellerDashboard";
+import SellerStatistic from "./SellerSide/SellerPages/SellerStatistics/SellerStatistics";
+import SellerUploadProduct from "./SellerSide/SellerPages/SellerUploadProduct/SellerUploadProduct";
+
 import SellerProducts from "./SellerSide/SellerPages/SellerProducts/SellerProducts";
 
 function App() {
