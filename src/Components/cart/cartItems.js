4ef4363967a555373835a7ef651f0be8ea9b3768
98Kush <92854662+98Kush@users.noreply.github.com>
2024-04-10 13:22:18 +0530

    cartupdate

--- src/Components/cart/cartItems.js
+++ src/Components/cart/cartItems.js
@@ -1,5 +1,5 @@
 import React,{useContext} from 'react'
-import './cart.css'
+import './cartitems.css'
 import UnistorecontextProvider from '../../Context/unistorecontext';
 
 export const CartItems = () =>  {  
